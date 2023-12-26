bad645dfd7e9cac014dd3b0847ff29aac39bdcfe
latofat123 <154625682+latofat123@users.noreply.github.com>
2023-12-26 20:30:12 +0500

    master

---
+++ main.py
@@ -0,0 +1,12 @@
+sideA = float(input("Длина: "))
+sideB = float(input("Ширина: "))
+
+area = sideA * sideB;
+print("Площадь прямоугольника: ", area)
+
+#zadaniya2
+sideA = float(input("Длина: "))
+sideB = float(input("Ширина: "))
+
+perimetr = (sideA + sideB) * 2;
+print("Периметр прямоугольника: ", perimetr)
