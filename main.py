c6a1435b7e092435e800fb152f5a242c0b36cd90
latofat123 <154625682+latofat123@users.noreply.github.com>
2023-12-27 23:41:14 +0500

    master2

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
-sideA = float(input("Длина: "))
-sideB = float(input("Ширина: "))
+# # Сначала вводится число N,затем вводится ровно N целых чисел. Подсчитайте,сколько из них равны нулю,и выведите это количество
+n = int(input())
+count = 0
+for i in range(n):
+    a = int(input())
+    if a == 0:
+        count +=1
+    else:
+        count += 0
+print(count)
 
-area = sideA * sideB;
-print("Площадь прямоугольника: ", area)
+# ##  zdaniya 2
+# ## Вводится натуральное число X.Подсчитайте количество натуральных делителей числа X(включая 1 и само число).x≤2e9(2миллиарда)
+x = int(input())
+count = 0
+for i in range(1, x+1):
+    if x % i == 0:
+          count += 1
 
-#zadaniya2
-sideA = float(input("Длина: "))
-sideB = float(input("Ширина: "))
+print(count - 2)
 
-perimetr = (sideA + sideB) * 2;
-print("Периметр прямоугольника: ", perimetr)
+## Вводятся целые числа A и B. Гарантируется,что A≤B .Выведите все четные числа на заданном отрезке через пробел
+
+a = int(input())
+b = int(input())
+if a % 2 == 0:
+     a += 1
+     for i in range(a, b, 2):
+          print(i)
+else:
+     for i in range(a, b, 2):
+          print(i)
+
+
+
+
+
